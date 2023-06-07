0f90b99d8a967ec404b441bbff91305a699beefd
AdamBures <47189693+AdamBures@users.noreply.github.com>
2023-06-07 21:19:49 +0200

    Update app.py
    Fixed some issues

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import sqlite3
 from flask import Flask, jsonify, request, abort
+from contextlib import contextmanager
+from jsonschema import validate, ValidationError
 
 app = Flask(__name__)
 DATABASE = 'movies.db'
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['JSON_SORT_KEYS'] = False
 
+# Create the movies table if it doesn't already exist
 with sqlite3.connect(DATABASE) as conn:
     conn.execute('''CREATE TABLE IF NOT EXISTS movies
     (id INTEGER PRIMARY KEY,
@@ -14,9 +17,34 @@ with sqlite3.connect(DATABASE) as conn:
     release_year INTEGER NOT NULL)''')
 
 
+@contextmanager
+def connect_db():
+    conn = sqlite3.connect(DATABASE)
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def validate_movie_data(data):
+    schema = {
+        'type': 'object',
+        'properties': {
+            'title': {'type': 'string'},
+            'description': {'type': 'string'},
+            'release_year': {'type': 'integer'}
+        },
+        'required': ['title', 'release_year']
+    }
+    try:
+        validate(data, schema)
+    except ValidationError as e:
+        abort(400, str(e))
+
+
 @app.route('/movies')
 def get_movies():
-    with sqlite3.connect(DATABASE) as conn:
+    with connect_db() as conn:
         cur = conn.cursor()
         cur.execute('SELECT id, title, description, release_year FROM movies')
         rows = cur.fetchall()
@@ -26,7 +54,7 @@ def get_movies():
 
 @app.route('/movies/<int:id>')
 def get_movie(id):
-    with sqlite3.connect(DATABASE) as conn:
+    with connect_db() as conn:
         cur = conn.cursor()
         cur.execute('SELECT * FROM movies WHERE id = ?', (id,))
         row = cur.fetchone()
@@ -39,35 +67,30 @@ def get_movie(id):
 
 @app.route('/movies', methods=['POST'])
 def create_movie():
-    if not request.json or not 'title' in request.json or not 'release_year' in request.json:
-        abort(400)
-    movie = {
-        'title': request.json['title'],
-        'description': request.json.get('description', ''),
-        'release_year': request.json['release_year'],
-    }
-    with sqlite3.connect(DATABASE) as conn:
+    data = request.get_json()
+    validate_movie_data(data)
+
+    with connect_db() as conn:
         cur = conn.cursor()
-        cur.execute('INSERT INTO movies (title, description, release_year) VALUES (?, ?, ?)', (movie['title'], movie['description'], movie['release_year']))
-        movie['id'] = cur.lastrowid
+        cur.execute('INSERT INTO movies (title, description, release_year) VALUES (?, ?, ?)',
+                    (data['title'], data.get('description', ''), data['release_year']))
+        movie_id = cur.lastrowid
         conn.commit()
+
+    movie = {'id': movie_id, 'title': data['title'], 'description': data.get('description', ''), 'release_year': data['release_year']}
     return jsonify(movie), 201
 
 
 @app.route('/movies/<int:id>', methods=['PUT'])
 def update_movie(id):
     data = request.get_json()
-    conn = sqlite3.connect('movies.db')
-    cur = conn.cursor()
-    cur.execute('UPDATE movies SET title = ?, description = ?, release_year = ? WHERE id = ?', (
-        data.get('title', None),
-        data.get('description', None),
-        data.get('release_year', None),
-        id
-    ))
+    validate_movie_data(data)
 
-    conn.commit()
-    conn.close()
+    with connect_db() as conn:
+        cur = conn.cursor()
+        cur.execute('UPDATE movies SET title = ?, description = ?, release_year = ? WHERE id = ?',
+                    (data['title'], data.get('description', ''), data['release_year'], id))
+        conn.commit()
 
     return jsonify(data), 201
 
